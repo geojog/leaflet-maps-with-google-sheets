df62ee82025e2a6eab5e9d718b8c1b08e4910c01
geojog <dben2007@gmail.com>
2023-07-21 15:49:57 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1BhLtuwFopjcsigHz0mlP75gleSSjDTy9kHZkuGQBGOY/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyBQ-5LXrYmyA28e-Qga2pNTTlu1dd7ckzQ';
